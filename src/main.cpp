57d1f480883baa07518aba4c87c0f659b43165cc
viktordicer <viktor.dicer@gmail.com>
2021-10-08 10:57:34 +0200

    configuration + debug mode serialPrint

--- src/main.cpp
+++ src/main.cpp
@@ -28,77 +28,36 @@
 #include <Ethernet.h>  //ETHERNET LIBRARY
 #include "PubSubClient.h" //MQTT LIBRARY
 #include "ClosedCube_HDC1080.h" // HDC1080
+#include "configuration.h"
 
+// DEBUG MODE DEFINE - in debug mode serial print is on
+//#define DEBUG
 
 EthernetClient ethClient;
+IPAddress ip(IP_ADDRESS[0], IP_ADDRESS[1], IP_ADDRESS[2], IP_ADDRESS[3]);    
 PubSubClient mqttClient(ethClient);
 ClosedCube_HDC1080 hdc1080; //temperature sensor
-
-
-//Objects
-
-//Variables
-
-#define RESET_PIN       PA0  //Reset pin for W5500
-
-#define MQTT_CLIENT_ID  "child3" //MQTT id client
-#define MQTT_USER       "viktor"
-#define MQTT_PASSWORD   "viktor"
-
-#define INTERVAL        20*60000 // 15 min delay between publishing
-
-//IPAddress ip(192, 168, 0, 195); //Foyer room
-//IPAddress ip(192, 168, 0, 170); //Child1 room196
-//IPAddress ip(192, 168, 0, 197); //Child2 room
-//IPAddress ip(192, 168, 0, 198); //Bath room
-//IPAddress ip(192, 168, 0, 199); //Technical room
-//IPAddress ip(192, 168, 0, 200); //Hall room
-IPAddress ip(192, 168, 0, 201); //Child3 room
-//IPAddress ip(192, 168, 0, 202); //Bed room
-//IPAddress ip(192, 168, 0, 158); //Office room
-//IPAddress ip(192, 168, 0, 159); //Larder room
-//IPAddress ip(192, 168, 0, 160); //Kitchen room
-//IPAddress ip(192, 168, 0, 161); //Living room 
-
-
-//uint8_t mac[6] = {0x02,0xA1,0x02,0x03,0x04,0x05}; //Foyer room
-//uint8_t mac[6] = {0x02,0x63,0x02,0x03,0x04,0x05}; //Child1 room
-//uint8_t mac[6] = {0x02,0xA1,0x03,0x03,0x04,0x05}; //Child2 room
-//uint8_t mac[6] = {0x02,0xA1,0x04,0x03,0x04,0x05}; //Bath room
-//uint8_t mac[6] = {0x02,0xA1,0x05,0x03,0x04,0x05}; //Technical room
-//uint8_t mac[6] = {0x02,0xA1,0x06,0x03,0x04,0x05}; //Hall room
-uint8_t mac[6] = {0x02,0xA1,0x07,0x03,0x04,0x05}; //Child3 room
-//uint8_t mac[6] = {0x02,0xA1,0x08,0x03,0x04,0x05}; //Bed room
-//uint8_t mac[6] = {0x02,0xA1,0x09,0x03,0x04,0x05}; //Office room
-//uint8_t mac[6] = {0x02,0xA1,0x01,0x04,0x04,0x05}; //Larder room
-//uint8_t mac[6] = {0x02,0xA1,0x02,0x04,0x04,0x05}; //Kitchen room
-//uint8_t mac[6] = {0x02,0xA1,0x04,0x04,0x04,0x05}; //Living room
-
-const char* mqtt_server = "192.168.0.107";
 
 //Sensor varialbles
 char temp[10];
 char hum[10];
 
-//MQTT topics
-const char* tempTopic = "sensor/child3/temperature";
-const char* humTopic = "sensor/child3/humidity";
-
-
+// Methodes
 void sendData();     
 void resetEthernet();
 
-//////////////////////// SETUP ///////////////////////
 void setup() {
-  // setup serial communication
-  Serial.begin(57600);
-  Serial.println("Starting...");
+  #ifdef DEBUG
+    Serial.begin(57600);
+    delay(4000);
+    Serial.println("STM32 starting...");
+    Serial.println("Debug mode");
+  #endif 
 
   //Initialize hdc1080
   hdc1080.begin(0x40);
 }
 
-//////////////////////// LOOP  ///////////////////////
 void loop() {
 
   //Read temperature end humidity
@@ -122,25 +81,22 @@ void sendData() {
         connectionAtempt = 1;
       }
       Ethernet.maintain();
-      Serial.println("Attempting MQTT connection...");
-      // Attempt to connect
-      Serial.print("IP address: ");
-      Serial.println(Ethernet.localIP());
+      serialPrint("Attempting MQTT connection...");
 
       if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD )) {
         //Publish MQTT
-        Serial.println("connected");
+        serialPrint("connected");
         mqttClient.publish(tempTopic,temp);
         mqttClient.publish(humTopic,hum);
         
       } else {
-        Serial.println("failed, rc=");
-        Serial.println(mqttClient.state());
-        Serial.println(" try again in 2 seconds");
+        serialPrint("failed, rc=");
+        serialPrint(mqttClient.state());
+        serialPrint(" try again in 2 seconds");
         
         delay(2000);
         Serial.print("Connection atempt: "); 
-        Serial.println(connectionAtempt);
+        serialPrint(connectionAtempt);
 
         connectionAtempt ++;
       }
@@ -148,19 +104,12 @@ void sendData() {
   }
   //mqttClient.loop();
   delay(200);
-  Serial.println("Connected to the mqtt");
   mqttClient.disconnect();
-  delay(200);
-  Serial.println("Client has stoped");
-  delay(200);
-  pinMode(RESET_PIN, OUTPUT);
-  digitalWrite(RESET_PIN, LOW);
-  Serial.println("Ethernet adapter has turned off");
 }
 
 void resetEthernet() {
   //Define reset pin for W5500
-  Serial.println("Reseting ethernet adapter");
+  serialPrint("Reseting ethernet adapter");
   pinMode(RESET_PIN, OUTPUT);
   digitalWrite(RESET_PIN, LOW);
   delay(100);
@@ -168,8 +117,8 @@ void resetEthernet() {
   delay(100);
   pinMode(RESET_PIN, INPUT);
   delay(1500);
-  Serial.println("Initialize ethernet");
-  Ethernet.begin(mac,ip);
+  serialPrint("Initialize ethernet");
+  Ethernet.begin(MAC,ip);
 
   //delay with ethernet maintain
   for (int i = 0; i <= 5; i++) {
@@ -178,5 +127,12 @@ void resetEthernet() {
   }
   
   mqttClient.setServer(mqtt_server,1883);
-  Serial.println(Ethernet.localIP());
+  serialPrint(Ethernet.localIP());
+}
+
+// Serial print if debug mode is on
+void serialPrint(String message){
+  #ifdef DEBUG
+    Serial.println(message);
+  #endif
 }
